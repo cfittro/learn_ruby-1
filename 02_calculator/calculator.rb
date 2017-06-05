82a53e1d0e17c3e9aa39f03776a1b4512322e433
cfittro <cdfittro@gmail.com>
2017-06-05 14:12:16 -0700

    02 passed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add (num1, num2)
+    num1 + num2
+end
+
+def subtract (num3, num4)
+    num3 - num4
+end
+
+def sum (numbers)
+    total = 0
+    numbers.each { |num| total = total + num}
+    total
+end
+
+def multiply (multiples)
+    total = 1
+    multiples.each { |num| total = total * num}
+    total
+end
+
+def power (base, exp)
+    newnum=base
+    while exp>1 do
+        newnum = newnum*base
+        exp = exp-1
+    end
+    newnum
+end
+
+def factorial (fact)
+    if fact <=1
+        return 1
+    else
+        fact * factorial(fact-1)
+    end
+end
