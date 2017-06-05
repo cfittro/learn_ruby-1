d98db074b291123d0f8a7fc9f342b5ee6d40218c
cfittro <cdfittro@gmail.com>
2017-06-05 15:08:24 -0700

    03 - passed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
-#write your code here
+def echo (str)
+    return str.to_s
+end
+
+def shout (txt)
+    return txt.to_s.upcase
+end
+
+def repeat (words, num=2)
+    return ([words]*num).join (' ')
+end
+
+def start_of_word (text, val)
+    text[0...val]
+end
+
+def first_word (multiple)
+    multiple.split.first
+end
+
+def titleize(str)
+  little_words = ['a,' 'an', 'and', 'the', 'but', 'or', 'for', 'nor',
+    'on', 'at', 'to', 'from', 'by', 'over']
+  words = str.split
+
+  new_str = "#{words[0].capitalize} "
+  words[1...(words.length)].each do |w|
+    if (little_words.include? w)
+      new_str << "#{w} "
+    else
+      new_str << "#{w.capitalize} "
+    end
+  end
+  new_str[0..-2]
+end
