cda205191b679029b6e2c25a86d32ab250fbd73b
cfittro <cdfittro@gmail.com>
2017-06-29 10:35:07 -0700

    finished lesson 6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+
+    attr_accessor :title
+
+    def title
+        words = @title.split(' ')
+        words.each do |word|
+            donotcap = ['a', 'an', 'the', 'and', 'but', 'or', 'nor', 'for', 'yet', 'in', 'of'] #etc
+            if donotcap.include? word
+                word
+            else
+                word.capitalize!
+            end
+        end
+    words[0].capitalize!    
+    @title = words.join(' ')
+    end
 end
