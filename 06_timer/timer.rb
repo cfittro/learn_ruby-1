5e237641fb1357476aba23fe8586ee33f392b488
cfittro <cdfittro@gmail.com>
2017-06-29 10:57:40 -0700

    finished final lesson

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    sec = @seconds
+    hours = sec / 3600
+    minutes = (sec % 3600) / 60
+    secleft = sec % 60
+    timer = "#{padded(hours)}:#{padded(minutes)}:#{padded(secleft)}"
+  end
+
+  def padded(num)
+    if num < 10
+      return "0#{num}"
+    elsif num.to_s.length == 2
+      return "#{num}"
+    end
+  end
+
 end
